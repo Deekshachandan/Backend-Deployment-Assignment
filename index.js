c68ea519758e33948a81885426ad5bc88b9d3a57
Deekshachandan <deeksharajpurohit15@gmail.com>
2023-03-13 23:42:04 +0530

    added changes

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ require('dotenv').config()
 const {noteRouter}=require("./Controller/note")
 
 const cors = require('cors');
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
+// const corsOptions ={
+//     origin:'*', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200,
+//  }
 const app=express()
+app.use(cors())
 app.use(express.json())
 
 app.use("/notes", noteRouter)
-app.use(cors(corsOptions))
 
 
 
